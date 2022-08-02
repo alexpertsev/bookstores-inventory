7e969a9e1c00054218cfb771726b1cc3903a79e6
alexpert <alex.pertsev@gmail.com>
2022-08-01 19:08:01 -0500

    Display stock levels changes via turbo streams

--- app/models/inventory_level.rb
+++ app/models/inventory_level.rb
@@ -1,6 +1,11 @@
 class StockLevelUpdateException < StandardError; end
 
 class InventoryLevel < ApplicationRecord
+
+  after_update_commit ->(inventory_level) { broadcast_replace_later_to *broadcast_params(inventory_level) }
+  after_create_commit ->(inventory_level) { broadcase_prepend_later_to *broadcast_params(inventory_level) }
+  after_destroy_commit ->(inventory_level) { broadcast_remove_later_to *broadcast_params(inventory_level) }
+
   belongs_to :bookstore_book
   MAX_STOCK = 1000000
 
@@ -16,11 +21,23 @@ class InventoryLevel < ApplicationRecord
 
   def remove_from_stock!(value)
     self.with_lock do
-      if value < self.stock_level
+      if value <= self.stock_level
         self.update(stock_level: (self.stock_level -= value))
       else
         raise StockLevelUpdateException, 'Cannot remove from stock more than it is available.'
       end
     end
   end
+
+  protected
+
+  def broadcast_params(inventory_level)
+    [
+      inventory_level.bookstore_book.bookstore,
+      partial: "bookstore_books/bookstore_book",
+      locals: { bookstore_book: inventory_level.bookstore_book },
+      target: inventory_level.bookstore_book
+    ]
+  end
+
 end
